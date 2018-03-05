3fd763e129a41e7ee2526cd2b4c80d41735fb904
UseLib <34091189+uselibrary@users.noreply.github.com>
2018-03-05 16:16:22 +0800

    Fix bugs
    Fix bugs

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # KMS Server
 
-`version 0.3`
+`version 0.4`
 
 It is a Shell Script which can build a KMS Server automatically.
 
 The following hardware platforms and operating systems are supported. More  hardware platforms and operating systems are coming.
 
-| Hardware Platform | Operating System     |
-| ----------------- | -------------------- |
-| x86/i386          | Debian、Ubuntu、CentOS |
-| x86-64/amd64      | Debian、Ubuntu、CentOS |
-| Raspberry pi      | Raspbian             |
+| Hardware Platform | Operating System            |
+| ----------------- | --------------------------- |
+| x86/i386          | Debian、Ubuntu、CentOS        |
+| x86-64/amd64      | Debian、Ubuntu、CentOS、Deepin |
+| Raspberry pi      | Raspbian                    |
 
 
 
--- kms.sh
+++ kms.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #URL: https://github.com/uselibrary/KMS_Server
-#E-mail: emkqson@gmail.com
+#E-mail: mail@pa.ci
 clear
 echo "    ################################################"
 echo "    #                                              #"
 echo "    #               Build KMS Server               #"
 echo "    #                https://pa.ci                 #"
-echo "    #                 Version 0.3                  #"
+echo "    #                 Version 0.4                  #"
 echo "    ################################################"
 #Prepare the installation environment
 echo -e ""
@@ -14,7 +14,7 @@ echo -e "Prepare the installation environment."
 if cat /etc/*-release | grep -Eqi "centos|red hat|redhat"; then
   echo "RPM-based"
   yum -y install git
-elif cat /etc/*-release | grep -Eqi "debian|ubuntu"; then
+elif cat /etc/*-release | grep -Eqi "debian|ubuntu|deepin"; then
   echo "Debian-based"
   apt-get -y install git
 else
@@ -29,35 +29,31 @@ else
 fi
 #Build KMS Server
 git clone https://github.com/uselibrary/KMS_Server
-mv KMS_Server vlmcsd
-mv vlmcsd /usr/local/
-mkdir /usr/local/KMS/
-ln -sv /usr/local/vlmcsd/ /usr/local/KMS/
 if cat /etc/*-release | grep -Eqi "raspbian"; then
-  echo "export PATH=/usr/local/KMS/vlmcsd/binaries/Linux/arm/little-endian/glibc:\$PATH" > /etc/profile.d/vlmcs.sh
-  source /etc/profile.d/vlmcs.sh
-  chmod +x /usr/local/KMS/vlmcsd/binaries/Linux/arm/little-endian/glibc/*
-  echo "nohup vlmcsd-armv6hf-Raspberry-glibc &" >> /etc/rc.local
-  nohup vlmcsd-armv6hf-Raspberry-glibc &
+  cd KMS_Server/binaries/Linux/arm/little-endian/glibc/
+  cp vlmcsd-armv6hf-Raspberry-glibc kms
 else
-  echo "export PATH=/usr/local/KMS/vlmcsd/binaries/Linux/intel/static:\$PATH" > /etc/profile.d/vlmcs.sh
-  source /etc/profile.d/vlmcs.sh
-  chmod +x /usr/local/KMS/vlmcsd/binaries/Linux/intel/static/*
+  cd KMS_Server/binaries/Linux/intel/glibc/
   if [ "$arch" -eq 32 ]; then
-    echo "nohup vlmcsd-x86-musl-static &" >> /etc/rc.local
-    nohup vlmcsd-x86-musl-static &
+    mv vlmcsd-x32-glibc kms
   else
-    echo "nohup vlmcsd-x64-musl-static &" >> /etc/rc.local
-    nohup vlmcsd-x64-musl-static &
+    mv vlmcsd-x64-glibc kms
   fi
 fi
-#Check vlmcsd status
+mv kms /usr/bin/
+chmod +x /usr/bin/kms
+kms
+echo "kms" >> /etc/rc.local
+#Cleaning Work
+cd -
+rm -rf KMS_Server
+#Check kms server status
 sleep 1
-echo "Check vlmcsd status..."
+echo "Check KMS Server status..."
 sleep 1
-PIDS=`ps -ef |grep vlmcsd |grep -v grep | awk '{print $2}'`
+PIDS=`ps -ef |grep kms |grep -v grep | awk '{print $2}'`
 if [ "$PIDS" != "" ]; then
-  echo "vlmcsd is runing!"
+  echo "kms server is runing!"
 else
-  echo "vlmcsd is NOT running!"
+  echo "kms server is NOT running!"
 fi
